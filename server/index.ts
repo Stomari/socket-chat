0e20ccc1c4849527b2dfb08e676d6b034ddfec59
Sergio Tomari <cerjo@MacBook-Air-de-Sergio.local>
2021-07-25 21:55:43 -0300

    feature: add client functionalities

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,28 @@
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import dotenv from 'dotenv';
+
+import logger from './utils/logger';
+
+dotenv.config();
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  // ...
+  cors: {
+    origin: [
+      'http://localhost:3001',
+    ],
+  },
 });
 
-io.on("connection", (socket: Socket) => {
-  // ...
+io.on('connection', (socket: Socket) => {
+  logger.info(`Connected to socket ${socket.id}`);
+
+  socket.on('SEND_MESSAGE', (data) => {
+    logger.info('Message received', { data });
+    io.emit('NEW_MESSAGE', data);
+  });
 });
 
-httpServer.listen(3000);
+httpServer.listen(process.env.PORT);
+logger.info(`Server listening to port ${process.env.PORT}`);
